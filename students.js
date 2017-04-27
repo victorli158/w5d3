c655785572b29f8b1db346158144e3cc16fd51d7
Victor Li <victor.li158@gmail.com>
2017-04-26 17:48:59 -0700

    Student.prototype.hasConflict function

--- students.js
+++ students.js
@@ -9,7 +9,7 @@ Student.prototype.name = function () {
 };
 
 Student.prototype.enroll = function (course) {
-  if (this.courses.includes(course) === false){
+  if (this.courses.includes(course) === false && !this.hasConflict(course)) {
     this.courses.push(course);
     course.students.push(this);
   }
@@ -27,6 +27,15 @@ Student.prototype.courseLoad = function () {
   });
 
   return result;
+};
+
+Student.prototype.hasConflict = function(otherCourse) {
+  this.courses.forEach(function(course) {
+    if (course.conflictsWith(otherCourse)) {
+      throw "Conflict!";
+    }
+  });
+  return false;
 };
 
 function Course(name, department, credits, block, days) {
@@ -60,13 +69,6 @@ let c1 = new Course("Ruby", "pool", 20, 1, ['mon', 'wed', 'fri']);
 let c2 = new Course("Rails", "hotel", 10, 2, ['mon', 'wed', 'fri']);
 let c3 = new Course("JavaScript", "pool", 15, 1, ['mon', 'tue']);
 let c4 = new Course("Java", "hotel", 5, 1, ['tue', 'thur']);
-
-s1.enroll(c1);
-s1.enroll(c2);
-s1.enroll(c3);
-
-s2.enroll(c1);
-s2.enroll(c2);
 
 // console.log(c1.conflictsWith(c4));
 // console.log(c1.conflictsWith(c3));
